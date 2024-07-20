353e490c4c033bee7f049463002beaec01dbc376
goodUserName11 <kireevkonstantin11@gmail.com>
2024-07-20 13:56:31 +0400

    Добавьте файлы проекта.

---
+++ Docs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Docs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

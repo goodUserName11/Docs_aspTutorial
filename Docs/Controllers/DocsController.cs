cc36b46447e2b4c21d32f4dc4f6cb0c244ee1b19
goodUserName11 <kireevkonstantin11@gmail.com>
2024-07-22 18:47:29 +0400

    Added docs controller and default pages

---
+++ Docs/Controllers/DocsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Docs.Data;
+using Docs.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Docs.Controllers
+{
+    [Authorize]
+    public class DocsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DocsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Docs
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Documents.Include(d => d.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Docs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doc = await _context.Documents
+                .Include(d => d.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (doc == null)
+            {
+                return NotFound();
+            }
+
+            return View(doc);
+        }
+
+        // GET: Docs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Docs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Content,UserId")] Doc doc)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(doc);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", doc.UserId);
+            return View(doc);
+        }
+
+        // GET: Docs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doc = await _context.Documents.FindAsync(id);
+            if (doc == null)
+            {
+                return NotFound();
+            }
+            if (doc.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+                return NotFound();
+            }
+
+            return View(doc);
+        }
+
+        // POST: Docs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,UserId")] Doc doc)
+        {
+            if (id != doc.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(doc);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DocExists(doc.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", doc.UserId);
+            return View(doc);
+        }
+
+        // GET: Docs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var doc = await _context.Documents
+                .Include(d => d.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (doc == null)
+            {
+                return NotFound();
+            }
+            if (doc.UserId != User.FindFirstValue(ClaimTypes.NameIdentifier))
+            {
+                return NotFound();
+            }
+
+            return View(doc);
+        }
+
+        // POST: Docs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var doc = await _context.Documents.FindAsync(id);
+            if (doc != null)
+            {
+                _context.Documents.Remove(doc);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DocExists(int id)
+        {
+            return _context.Documents.Any(e => e.Id == id);
+        }
+    }
+}

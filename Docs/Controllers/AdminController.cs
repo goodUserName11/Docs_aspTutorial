3510abb7405f979f3260bf054c929a1101a8ba25
goodUserName11 <kireevkonstantin11@gmail.com>
2024-07-31 11:49:01 +0400

    Added admin page and controller

---
+++ Docs/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Docs.Data;
+using Docs.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Docs.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        RoleManager<IdentityRole> _roleManager;
+        UserManager<IdentityUser> _userManager;
+
+        public AdminController(ApplicationDbContext context, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        // GET: Admin
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
